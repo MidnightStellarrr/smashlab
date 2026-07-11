96c1cf53331ff0c9302e6c28842964227aa4da13
MidnightStellarrr <hannahgranada458@gmail.com>
2026-07-11 21:15:35 +0800

    interactive map - done

--- resources/views/user/contact.blade.php
+++ resources/views/user/contact.blade.php
@@ -155,5 +155,22 @@
     </div>
 
 </section>
+
+<!-- ===========================
+        MAP SECTION - FULL WIDTH
+============================ -->
+
+<section class="map-section">
+    <div class="map-container">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30880.60284265476!2d121.03560675!3d14.6760414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b7e13f1c0f0d%3A0xd5b75d0a3e8f2e3!2sQuezon%20City%2C%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1700000000000"
+            width="100%"
+            height="400"
+            style="border:0; display:block;"
+            loading="lazy"
+            allowfullscreen>
+        </iframe>
+    </div>
+</section>
 </body>
 </html>
