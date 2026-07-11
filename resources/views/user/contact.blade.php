4a5fe664a9545d19619db924863e5b1406b90128
MidnightStellarrr <hannahgranada458@gmail.com>
2026-07-11 10:52:05 +0800

    addes footer into partials and then include it to app.blade.php

--- resources/views/user/contact.blade.php
+++ resources/views/user/contact.blade.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Contact</title>
 </head>
 <body>
-    contact
+    @include('user.partials.hero', [
+    'mainHeading' => 'Contact',
+    'subHeading' => 'Have questions? We\'d love to hear from you.'
+    ])
+
 </body>
 </html>
