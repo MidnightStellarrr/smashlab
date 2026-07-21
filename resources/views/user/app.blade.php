b8c26c53862d3e0ec9b31cf4f811379f7a0cb7a6
MidnightStellarrr <hannahgranada458@gmail.com>
2026-07-21 22:28:25 +0800

    added initial darkmode feature but still need adjustemnts

--- resources/views/user/app.blade.php
+++ resources/views/user/app.blade.php
@@ -800,6 +800,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+
+(function() {
+        const storedTheme = localStorage.getItem('smashlab-theme');
+        if (storedTheme === 'dark') {
+            document.documentElement.classList.add('dark');
+        } else if (storedTheme === 'light') {
+            document.documentElement.classList.remove('dark');
+        } else {
+            const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+            if (prefersDark) {
+                document.documentElement.classList.add('dark');
+            }
+        }
+    })();
 </script>
 
 </body>
